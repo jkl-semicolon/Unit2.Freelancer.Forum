83618bf6ec803a099d08e15a997f188cd518183d
jkl <alexwang925@gmail.com>
2024-03-14 17:53:15 -0300

    css added

---
+++ main.js
@@ -0,0 +1,14 @@
+const freelancersArray = [
+  {name: 'Alice', occupation: 'Writer', startingPrice: '$30',},
+  {name: 'Bob', occupation: 'Teacher', startingPrice: '$50',},
+  {name: 'Carol', occupation: 'Programmer', startingPrice: '$70',},
+  {name: 'Darlene', occupation: 'Astronaut', startingPrice: '$200',},
+  {name: 'Edmund', occupation: 'Scam Artist', startingPrice: '$0',},
+  {name: 'Fitzgerald', occupation: 'Ballerina', startingPrice: '$60',},
+  {name: 'Gerard', occupation: 'Fixer', startingPrice: '$2000',},
+  {name: 'Hayley', occupation: 'Thief', startingPrice: '$10',},
+  {name: 'Iris', occupation: 'Chef', startingPrice: '$40',},
+  {name: 'Julius', occupation: 'Caesar', startingPrice: '$300',}
+]
+
+console.log(freelancersArray);
